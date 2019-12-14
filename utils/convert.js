4c3f2c3be130e8bbea18de94f2e13ee3d46a642d
pacificescape <17690123+pacificescape@users.noreply.github.com>
2019-12-14 15:28:41 +0100

    parseMain

--- utils/convert.js
+++ utils/convert.js
@@ -1,21 +1,9 @@
 const sharp = require('sharp')
 
 module.exports = async (webpBuffer) => {
-  // let stream = fs.createWriteStream('png.png')
-  // let buffers = []
-
   const bufferPng = await sharp(webpBuffer)
     .png()
     .toBuffer()
-
-  // stream.on('data', (data) => {
-  //   // console.log(data)
-  //   buffers.push(data)
-  // })
-  // stream.on('end', () => {
-  //   console.log('stream end')
-  //   resolve(Buffer.concat(buffers))
-  // })
 
   return bufferPng
 }
