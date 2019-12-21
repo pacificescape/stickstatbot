5836e747d72b1a54627a506800ff611a389fb677
pacificescape <17690123+pacificescape@users.noreply.github.com>
2019-12-21 04:35:32 +0100

    0.1.61

--- bot/handlers/getStats.js
+++ bot/handlers/getStats.js
@@ -1,5 +1,6 @@
 module.exports = async (ctx) => {
   let user = await ctx.db.User.findOne({ telegram_id: ctx.chat.id })
+  if (!user) return ctx.reply('Сначала добавьте стикерпак')
 
   user.stickerpacks.map((name, i) => {
     setTimeout(() => {
