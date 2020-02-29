3ee0e18152cec2db37fd3a9e3b9454a0faf1dbc4
Lord1Luciani <17690123+Lord1Luciani@users.noreply.github.com>
2020-02-29 18:08:56 +0100

    for of in getStats handler

--- bot/handlers/getStats.js
+++ bot/handlers/getStats.js
@@ -2,10 +2,8 @@ module.exports = async (ctx) => {
   let user = await ctx.db.User.findOne({ telegram_id: ctx.chat.id })
   if (!user) return ctx.reply('Сначала добавьте стикерпак')
 
-  user.stickerpacks.map((name, i) => {
-    setTimeout(() => {
-      ctx.reply('/packstats')
-        .then(() => ctx.reply(name))
-    }, 230 * i)
-  })
+  for (const pack of user.stickerpacks) {
+    await ctx.reply('/packstats')
+    await ctx.reply(pack)
+  }
 }
