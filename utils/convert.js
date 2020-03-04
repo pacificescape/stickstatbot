c9fa1c4db51ed73ee141833711a9cc992db655bb
Lord1Luciani <17690123+Lord1Luciani@users.noreply.github.com>
2020-03-04 02:55:53 +0100

    resize sharp

--- utils/convert.js
+++ utils/convert.js
@@ -2,7 +2,12 @@ const sharp = require('sharp')
 
 module.exports = async (webpBuffer) => {
   const bufferPng = await sharp(webpBuffer)
-    .png()
+    .png({
+      quality: 95
+    })
+    .resize(150, 150, {
+      fit: 'inside'
+    })
     .toBuffer()
     .catch(error => console.log(error))
 
